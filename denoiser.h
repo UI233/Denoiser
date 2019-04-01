48bdb675185a7184dfcb81b85fdfe23c6f5ae374
unknown <cjjcanrigou@163.com>
2019-04-01 08:25:54 +0800

    Complete the project

---
+++ denoiser.h
@@ -0,0 +1,12 @@
+#ifndef DENOISER_H
+#define DENOISER_H
+
+#include "Denoising/Algorithms/BilateralMeshDenoising.h"
+#include "Denoising/Algorithms/Noise.h"
+#include "Denoising/Algorithms/MeshDenoisingViaL0Minimization.h"
+#include "Denoising/Algorithms/BilateralNormalFilteringForMeshDenoising.h"
+#include "Denoising/Algorithms/GuidedMeshNormalFiltering.h"
+#include "Denoising/Algorithms/NonIterativeFeaturePreservingMeshFiltering.h"
+#include "Denoising/Algorithms/FastAndEffectiveFeaturePreservingMeshDenoising.h"
+
+#endif // DENOISER_H
